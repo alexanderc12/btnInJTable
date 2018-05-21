28712d1fa594a48e9dba632f029cb833af704f0e
Alexander <alexanderc@outlook.com>
2018-05-21 17:11:26 -0500

    Creación del proyecto Codigo necesario para mostrar un boton en una tabla y administrar su evento.

---
+++ src/controllers/Controller.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+import views.TableWindow;
+
+public class Controller implements ActionListener{
+	
+	private TableWindow tableWindow;
+	
+	public Controller() {
+		tableWindow = new TableWindow(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		switch (Actions.valueOf(e.getActionCommand())) {
+			case ADD:
+				add();
+			break;
+			case DELETE:
+				executeAction(((JComponent)e.getSource()).getName());
+			break;
+		}
+	}
+	
+	private void add() {
+		String name = JOptionPane.showInputDialog(tableWindow, "Item: ");
+		tableWindow.addRegistryToTable(name, this);
+	}
+
+	private void executeAction(String id) {
+		JOptionPane.showMessageDialog(tableWindow, "Item: " + id,
+				"Item", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void main(String[] args) {
+		new Controller();
+	}
+}
